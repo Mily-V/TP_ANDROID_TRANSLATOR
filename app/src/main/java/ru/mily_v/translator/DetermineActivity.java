36e42a930e8417ea11e0a99c1304a30a262e1ccc
Сергей Титаевский <Titaevskiy.S@gmail.com>
2015-03-12 13:47:17 +0300

    Determine works

--- app/src/main/java/ru/mily_v/translator/DetermineActivity.java
+++ app/src/main/java/ru/mily_v/translator/DetermineActivity.java
@@ -6,21 +6,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class DetermineActivity extends Activity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DetermineActivity extends Activity implements TranslateTask.TranslateTaskListener {
 
 	public static final String EXTRA_EDIT_TEXT_RESULT = "extra_edit_text_result";
 	private EditText editTextForDetermine;
-	private EditText editTextForTranslate;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.determine);
 		editTextForDetermine = (EditText) findViewById(R.id.determine_edit_text_src);
-		editTextForTranslate = (EditText) findViewById(R.id.translate_edit_text_origin);
 
-		final Button goToTranslateActivity = (Button) findViewById(R.id.determine_button_translate);
+		findViewById(R.id.determine_button_determine)
+				.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						String origin = ((TextView) findViewById(R.id.determine_edit_text_src))
+								.getText().toString();
+						new TranslateTask(DetermineActivity.this, "detect")
+								.execute(origin);
+
+					}
+				});
+
+		final Button goToTranslateActivity = (Button) findViewById(R.id
+				.determine_button_to_translate);
 		goToTranslateActivity.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -29,5 +44,24 @@ public class DetermineActivity extends Activity {
 				startActivity(i);
 			}
 		});
+	}
+
+	@Override
+	public void onPostExecute(JSONObject json) {
+		findViewById(R.id.determine_progressbar).setVisibility(View.INVISIBLE);
+
+		String s = null;
+		try {
+			s = json.getString("lang");
+		}
+		catch (JSONException e) {
+			e.printStackTrace();
+		}
+		((EditText) findViewById(R.id.determine_edit_text_dest)).setText(s);
+	}
+
+	@Override
+	public void onPreExecute() {
+		findViewById(R.id.determine_progressbar).setVisibility(View.VISIBLE);
 	}
 }
